092531b0dae0d790efc29ce5cf0da4edeb96f8ed
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-11 13:31:47 +0300

    JDBC PP_1.1.4 сделано

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,26 +8,32 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-        UserDaoJDBCImpl.getInstance().createUsersTable();
+//        UserDaoJDBCImpl.getInstance().createUsersTable();
+        UserDaoHibernateImpl.getInstance().createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDaoJDBCImpl.getInstance().dropUsersTable();
+//        UserDaoJDBCImpl.getInstance().dropUsersTable();
+        UserDaoHibernateImpl.getInstance().dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDaoJDBCImpl.getInstance().saveUser(name, lastName, age);
+//        UserDaoJDBCImpl.getInstance().saveUser(name, lastName, age);
+        UserDaoHibernateImpl.getInstance().saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDaoJDBCImpl.getInstance().removeUserById(id);
+//        UserDaoJDBCImpl.getInstance().removeUserById(id);
+        UserDaoHibernateImpl.getInstance().removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return UserDaoJDBCImpl.getInstance().getAllUsers();
+//        return UserDaoJDBCImpl.getInstance().getAllUsers();
+        return UserDaoHibernateImpl.getInstance().getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDaoJDBCImpl.getInstance().cleanUsersTable();
+//        UserDaoJDBCImpl.getInstance().cleanUsersTable();
+        UserDaoHibernateImpl.getInstance().cleanUsersTable();
     }
 }
